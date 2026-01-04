029bbce2ca4060bb93b24a0ca8e531f8ed8d2119
Ibrahim Mahmoud <ibrahim.mahmoudd.ibrahim@gmail.com>
2026-01-04 14:08:43 +0200

    .

---
+++ sheets/assiut/01- Data type - Conditions /B-Basic Data Types.cpp
@@ -0,0 +1,26 @@
+/*
+Problem: B-Basic Data Types
+Source: Assiut Sheet - Data types & conditions 
+
+
+Pseudo-code:
+1- declration for (int i) (long long l) (char c) (float f) (double d) 
+2- take from the user all of these
+3- print each element on a new line in the same order it was received as input.
+*/
+
+
+#include<iostream>
+using namespace std;
+int main () {
+    int i ;
+    long long l ;
+    char  c ;
+    float f ;
+    double d ;
+    cin >>i>>l>>c>>f>>d ;
+    cout <<i<<"\n"<<l<<"\n"<<c<<"\n"<<f<<"\n"<<d ;
+    
+    
+    return 0;
+}
